de6f7eca7a15acca765fd360b70b2270a05fed58
EOO7 <ethan.oo333@gmail.com>
2017-05-14 19:03:54 +1000

    Added integer for number of moves made and arrow key input support
    Added integer for number of moves made and arrow key input support

--- src/ArrowKeyListener.java
+++ src/ArrowKeyListener.java
@@ -4,21 +4,27 @@ import java.awt.event.KeyListener;
 public class ArrowKeyListener implements KeyListener{
 	private int xCoord = 0;
 	private int yCoord = 0;
+	private int numMoves = 0;
+	
 	public void keyPressed(KeyEvent e){
 		int key = e.getKeyCode();
 		
 		if(key == KeyEvent.VK_W || key == KeyEvent.VK_UP){
 			this.yCoord = -1;
 			System.out.println("yCoord = " + yCoord);
+			numMoves++;
 		} else if(key == KeyEvent.VK_A || key == KeyEvent.VK_LEFT){
 			this.xCoord = -1;
 			System.out.println("XCoord = " + xCoord);
+			numMoves++;
 		} else if(key == KeyEvent.VK_S || key == KeyEvent.VK_DOWN){
 			this.yCoord = 1;
 			System.out.println("yCoord = " + yCoord);
+			numMoves++;
 		} else if(key == KeyEvent.VK_D || key == KeyEvent.VK_RIGHT){
 			this.xCoord = 1;
 			System.out.println("XCoord = " + xCoord);
+			numMoves++;
 		}
 	}
 	
