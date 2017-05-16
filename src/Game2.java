331e61c697a7337190370009217befae8b9c0781
Winson Wen <hestokaza@gmail.com>
2017-05-16 17:04:56 +1000

    Fixed up box and check win delay

--- src/Game2.java
+++ src/Game2.java
@@ -2,16 +2,16 @@ import java.util.Arrays;
 import java.util.Observable;
 
 public class Game2 extends Observable {
-	
-	//local stuff for the game
+
+	// local stuff for the game
 	private int[][] matrix;
 	private int[][] originalState;
 	private int[][] winningState;
 	private int numMoves;
 	private Player player;
 	private Boolean checkWin;
-	
-	//constructor
+
+	// constructor
 	public Game2() {
 		this.matrix = new int[][] {
 			{0,0,0,0,0,0,0,0,0,0,0},
@@ -40,34 +40,22 @@ public class Game2 extends Observable {
 		};
 		
 		this.player = new Player(1, 1);
-		//needs
-		//map - adj matrix
-		//player
-		//linkedlist of boxes
-		//linkedlist of crosses
+		// needs
+		// map - adj matrix
+		// player
+		// linkedlist of boxes
+		// linkedlist of crosses
 	}
-	
+
 	public int[][] getMatrix() {
 		return matrix;
 	}
 
-	//update the game state
-	//probs gets passed user key input
-	//then changes the game state
+	// update the game state
+	// probs gets passed user key input
+	// then changes the game state
 	public void update(int xCoord, int yCoord, char keyPress) {
-		
-		//maybe check for win first -> check if crosses and boxes share coords
-		checkWin = true;
-		for(int i = 0; i < winningState.length; i++){
-			if(!Arrays.equals(this.matrix[i], winningState[i])){
-				checkWin = false;
-			}
-		}
-		if(checkWin == true){
-			this.checkWin = true;
-			System.out.println("win");
-		}
-		//get user input
+		// get user input
 		int x = getXCoordinate();
 		int tempX = x;
 		int y = getYCoordinate();
@@ -76,95 +64,100 @@ public class Game2 extends Observable {
 		x += xCoord;
 		y += yCoord;
 		char key = keyPress;
-		if(matrix[y][x] == 0){
+		if (matrix[y][x] == 0) {
 			return;
-		} else if(matrix[y][x] == 2){
-			
-			if(key == 'W'){
-				if(matrix[y-1][x] == 0 || matrix[y-1][x] == 2){
+		} else if (matrix[y][x] == 2) {
+			if (key == 'W') {
+				if (matrix[y - 1][x] == 0 || matrix[y - 1][x] == 2) {
 					return;
 				}
-				matrix[y-1][x] = matrix[y][x];
-			//	matrix[y][x] = 1;
+				matrix[y - 1][x] = matrix[y][x];
+				matrix[y][x] = 4;
 				this.player.x = x;
 				this.player.y = y;
 				matrix[tempY][tempX] = originalState[tempY][tempX];
-				
-			} else if(key == 'A'){
-				if(matrix[y][x-1] == 0 || matrix[y][x-1] == 2){
+
+			} else if (key == 'A') {
+				if (matrix[y][x - 1] == 0 || matrix[y][x - 1] == 2) {
 					return;
 				}
-				matrix[y][x-1] = matrix[y][x];
-			//	matrix[y][x] = 1;
+				matrix[y][x - 1] = matrix[y][x];
+				matrix[y][x] = 4;
 				this.player.x = x;
 				this.player.y = y;
 				matrix[tempY][tempX] = originalState[tempY][tempX];
-			} else if(key == 'S'){
-				if(matrix[y+1][x] == 0 || matrix[y+1][x] == 2){
+			} else if (key == 'S') {
+				if (matrix[y + 1][x] == 0 || matrix[y + 1][x] == 2) {
 					return;
 				}
-				matrix[y+1][x] = matrix[y][x];
-			//	matrix[y][x] = 1;
+				matrix[y + 1][x] = matrix[y][x];
+				matrix[y][x] = 4;
 				this.player.x = x;
 				this.player.y = y;
 				matrix[tempY][tempX] = originalState[tempY][tempX];
-			} else if(key == 'D'){
-				if(matrix[y][x+1] == 0 || matrix[y][x+1] == 2){
+			} else if (key == 'D') {
+				if (matrix[y][x + 1] == 0 || matrix[y][x + 1] == 2) {
 					return;
 				}
-				matrix[y][x+1] = matrix[y][x];
-			//	matrix[y][x] = 1;
+				matrix[y][x + 1] = matrix[y][x];
+				matrix[y][x] = 4;
 				this.player.x = x;
 				this.player.y = y;
 				matrix[tempY][tempX] = originalState[tempY][tempX];
 			}
 		} else {
-			//	matrix[y][x] = 1;
 			this.player.x = x;
 			this.player.y = y;
 			matrix[tempY][tempX] = originalState[tempY][tempX];
 		}
+		checkWin();
 		this.numMoves++;
 		setChanged();
 		notifyObservers();
-		//parse input - menu,info,quit,reset,move
-		
-		//pass move to player -> player checks for collision, moves accordingly and updates position
-		// if collsion -> if box -> box checks for collision and player&box move and update positions
+		// parse input - menu,info,quit,reset,move
+
+		// pass move to player -> player checks for collision, moves accordingly
+		// and updates position
+		// if collsion -> if box -> box checks for collision and player&box move
+		// and update positions
 		// if collision with box and box cant move on changes are made
-		
-		//get coords for player and boxes, update the adj matrix with new coords
+
+		// get coords for player and boxes, update the adj matrix with new
+		// coords
 	}
-	
-	public int getXCoordinate(){
-	/*int x = 0;
-	for(int row = 0; row<this.matrix.length; row++){
-		for(int col = 0; col<this.matrix[0].length;col++){
-			if(this.matrix[row][col] == 1){
-				x = col;
-				break;
+
+	private void checkWin() {
+		// maybe check for win first -> check if crosses and boxes share coords
+		checkWin = true;
+		for (int i = 0; i < winningState.length; i++) {
+			if (!Arrays.equals(this.matrix[i], winningState[i])) {
+				checkWin = false;
 			}
 		}
+		if (checkWin == true) {
+			System.out.println("win");
+		}
 	}
-	return x;*/
+
+	public int getXCoordinate() {
+		/*
+		 * int x = 0; for(int row = 0; row<this.matrix.length; row++){ for(int
+		 * col = 0; col<this.matrix[0].length;col++){ if(this.matrix[row][col]
+		 * == 1){ x = col; break; } } } return x;
+		 */
 		return this.player.x;
 	}
-	
-	public int getYCoordinate(){
-	/*int y = 0;
-	for(int row = 0; row<this.matrix.length; row++){
-		for(int col = 0; col<this.matrix[0].length;col++){
-			if(this.matrix[row][col] == 1){
-				y = row;
-				break;
-			}
-		}
-	}
-	return y;*/
+
+	public int getYCoordinate() {
+		/*
+		 * int y = 0; for(int row = 0; row<this.matrix.length; row++){ for(int
+		 * col = 0; col<this.matrix[0].length;col++){ if(this.matrix[row][col]
+		 * == 1){ y = row; break; } } } return y;
+		 */
 		return this.player.y;
 	}
 
-	public int getNumMoves(){
+	public int getNumMoves() {
 		return this.numMoves;
 	}
 }
