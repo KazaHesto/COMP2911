efcdaf3d41f51423b831ce7d0f3e3a6cf391fbe1
Nanda <Nanda@DESKTOP-5063IMK>
2017-05-22 18:03:03 +1000

    broken save/data

---
+++ src/SaveData.java
@@ -0,0 +1,9 @@
+
+public class SaveData implements java.io.Serializable {
+
+    private static final long serialVersionUID = 1L;
+
+    public int[][] matrix;
+    public int row;
+    public int column;
+}
