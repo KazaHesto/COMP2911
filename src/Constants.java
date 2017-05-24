03720d034fe15e17cf1ab7eb126183c900698105
Winson Wen <hestokaza@gmail.com>
2017-05-24 15:03:18 +1000

    Move some stuff into constants

--- src/Constants.java
+++ src/Constants.java
@@ -6,4 +6,9 @@ public class Constants {
 	public static final String HELP_TEXT = "W - Forward\nA - Left\nS - Backwards\nD - Right\nAim : To successfully move all the Warehouse Boxes onto the Green Cross";
 	public static final String ABOUT_TITLE = "About WAREHOUSE BOSS";
 	public static final String ABOUT_TEXT = "IT'S A THING";
+
+	public static final int WALL = 1;
+	public static final int BOX = 2;
+	public static final int CROSS = 3;
+	public static final int FLOOR = 4;
 }
