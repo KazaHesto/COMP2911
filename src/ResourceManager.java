ad3fb93bc247cb23a3ed1dcdaa239a1b578b5845
EOO7 <ethan.oo333@gmail.com>
2017-05-26 21:24:20 +1000

    Java doc'd ResourceManager

--- src/ResourceManager.java
+++ src/ResourceManager.java
@@ -8,11 +8,15 @@ import java.nio.file.Paths;
 public class ResourceManager {
 
 	/**
-	 * @param data -> save data
-	 * @param file -> where you want to save the game
-	 * @throws Exception -> the game is not able to be saved
+	 * method: save() -> saves the game
+	 * 
+	 * @param 	data			save data
+	 * @param 	file			where you want to save the game
+	 * @return	-
+	 * @throws 	Exception		the game is not able to be saved
 	 */
 	public static void save(Serializable data, File file) throws Exception {
+		// save the game to a file
 		String filePath = file.getAbsolutePath();
 		if (!filePath.endsWith(".save")) {
 			filePath = filePath.concat(".save");
@@ -22,13 +26,15 @@ public class ResourceManager {
 		}
 	}
 	/**
+	 * method: load() -> loads a game based on a save file
 	 * 
-	 * @param file -> file that is to be loaded
-	 * @return
-	 * @throws Exception -> if the game is not able to be saved
+	 * @param 	file			file that is to be loaded
+	 * @return	-
+	 * @throws 	Exception		if the game is not able to be saved
 	 */
 
 	public static Object load(File file) throws Exception {
+		// load the save file
 		try (ObjectInputStream ois = new ObjectInputStream(Files.newInputStream(Paths.get(file.getAbsolutePath())))) {
 			return ois.readObject();
 		}
