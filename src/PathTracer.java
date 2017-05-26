401dfbf75b5c5f72e6e9b421a94839dae9491e71
EOO7 <ethan.oo333@gmail.com>
2017-05-26 22:19:41 +1000

    java doc'd PathTracer

--- src/PathTracer.java
+++ src/PathTracer.java
@@ -1,24 +1,52 @@
 
 public class PathTracer {
-
+	// declaring variables
+	
+	// the map
 	private int[][] matrix;
+	// a random variable for the x-coordinate
 	private int randx;
+	// the x-coordinate
 	private int x;
+	// the y-coordinate
 	private int y;
+	// a random variable for the y-coordinate
 	private int randy;
+	// the number of moves in the path
 	private int pathLength;
+	// an int representing direction
 	private int direction;
+	// the old direction
 	private int oldD;
 
+	/**
+	 * constructor
+	 * 
+	 * @param 	xsize	the horizontal size of the path
+	 * @param 	ysize	the vertical size of the path
+	 * @return	-
+	 * @throws	-
+	 */
 	public PathTracer(int xsize, int ysize) {
+		// initialise a matrix of the required size
 		this.matrix = new int[ysize][xsize];
 
 	}
 
+	/**
+	 * method: makePath() -> makes a path through the map
+	 * 
+	 * @param 	xsize	the horizontal size of the map
+	 * @param 	ysize	the vertical size of the map
+	 * @return	matrix	a path through the map
+	 */
 	public int[][] makePath(int xsize, int ysize) {
+		// initialise variables
 		int length = 0;
 		int i = 0;
 		int j = 0;
+		
+		// populate the matrix
 		while (i < ysize) {
 			j = 0;
 			while (j < xsize) {
@@ -27,13 +55,16 @@ public class PathTracer {
 			}
 			i++;
 		}
-
+		
+		// generate random numbers and assign them
 		this.randx = 2 + (int)(Math.random() * ((xsize - 5) + 1));
 		this.randy = 2 + (int)(Math.random() * ((ysize - 5) + 1));
 		this.x = this.randx;
 		this.y = this.randy;
 		this.pathLength = 10 + (int)(Math.random() * ((20 - 10) + 1));
 		this.direction = 1 + (int)(Math.random() * ((3 - 1) + 1));
+		
+		// generate a path
 		while (length <= ysize/2) {
 		
 		this.matrix[this.randy][this.randx] = 2;
@@ -52,64 +83,6 @@ public class PathTracer {
 		} else {
 			this.oldD = 3;
 		}
-		/*
-		int i = 0;
-		while (i < this.pathLength) {
-			switch (this.direction) {
-			case 1:
-				if (this.randy == 1) {
-					i--;
-					this.randy++;
-				} else {
-					this.randy--;
-				}
-				this.matrix[this.randy + 2][this.randx] = 4;
-				break;
-			case 2:
-				if (this.randx == xsize - 2) {
-					i--;
-					this.randx--;
-				} else {
-					this.randx++;
-				}
-				this.matrix[this.randy][this.randx - 2] = 4;
-				break;
-			case 3:
-				if (this.randy == ysize - 2) {
-					i--;
-					this.randy--;
-				} else {
-					this.randy++;
-				}
-				this.matrix[this.randy - 2][this.randx] = 4;
-				break;
-			case 4:
-				if (this.randx == 1) {
-					i--;
-					this.randx++;
-				} else {
-					this.randx--;
-				}
-				this.matrix[this.randy][this.randx + 2] = 4;
-				break;
-			}
-			this.matrix[this.randy][this.randx] = 4;
-			this.direction = 1 + (int) (Math.random() * ((4 - 1) + 1));
-
-			i++;
-		}
-		this.matrix[this.randy][this.randx] = 3;
-		this.matrix[this.y][this.x] = 2;
-		return matrix;
-		*/
-		
-		
-		/*
-		 * oldD: 1,2,3,4
-		 * 		up,right,down,left
-		 *  direction: 1,2,3
-		 *  	forward,right,left
-		 */
 		
 		i = 0;
 		while (i < this.pathLength) {			
